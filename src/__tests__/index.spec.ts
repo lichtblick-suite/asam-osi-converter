9dc116d4597ad177c2e7ed3630faf132d184f600
Thomas Sedlmayer <tsedlmayer@pmsfit.de>
2025-03-04 17:20:52 +0100

    test: extending mock objects
    Signed-off-by: Thomas Sedlmayer <tsedlmayer@pmsfit.de>

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,8 +1,10 @@
-import { ExtensionContext } from "@lichtblick/suite";
 import {
   GroundTruth,
+  Lane,
+  Lane_Classification_Type,
   LaneBoundary,
   LaneBoundary_BoundaryPoint,
+  LaneBoundary_Classification_Color,
   LaneBoundary_Classification_Type,
   MovingObject,
   MovingObject_Type,
@@ -13,6 +15,7 @@ import {
   MovingObject_VehicleClassification_Type,
   StationaryObject,
 } from "@lichtblick/asam-osi-types";
+import { ExtensionContext } from "@lichtblick/suite";
 import { DeepRequired } from "ts-essentials";
 
 import {
@@ -98,14 +101,56 @@ describe("OSI Visualizer: Message Converter", () => {
           z: 0,
         },
         width: 1,
+        height: 0,
       },
     ],
     classification: {
       type: {
         value: LaneBoundary_Classification_Type.NO_LINE,
       },
+      color: LaneBoundary_Classification_Color.WHITE,
     },
   } as unknown as DeepRequired<LaneBoundary>;
+  const mockLane = {
+    id: {
+      value: 1000,
+    },
+    classification: {
+      type: {
+        value: Lane_Classification_Type.DRIVING,
+      },
+      left_lane_boundary_id: [
+        {
+          value: 2,
+        },
+      ],
+      right_lane_boundary_id: [
+        {
+          value: 999,
+        },
+      ],
+      lane_pairing: [
+        {
+          antecessor_lane_id: {
+            value: 999,
+          },
+          successor_lane_id: {
+            value: 999,
+          },
+        },
+      ],
+      left_adjacent_lane_id: [
+        {
+          value: 999,
+        },
+      ],
+      right_adjacent_lane_id: [
+        {
+          value: 999,
+        },
+      ],
+    },
+  } as unknown as DeepRequired<Lane>;
   const mockMessageData = {
     timestamp: {
       seconds: 0,
@@ -117,6 +162,7 @@ describe("OSI Visualizer: Message Converter", () => {
     moving_object: [mockMovingObject],
     stationary_object: [mockStationaryObject],
     lane_boundary: [mockLaneBoundary],
+    lane: [mockLane],
     traffic_sign: [],
     traffic_light: [],
   } as GroundTruth;
@@ -187,6 +233,7 @@ describe("OSI Visualizer: Lane Boundaries", () => {
     const mockLaneBoundaryPoint = {
       position: { x: 0, y: 0, z: 0 },
       width: 2.0,
+      height: 0.0,
     } as DeepRequired<LaneBoundary_BoundaryPoint>;
     const mockLaneBoundary = {
       id: { value: 123 },
@@ -203,8 +250,16 @@ describe("OSI Visualizer: Lane Boundaries", () => {
           value: LaneBoundary_Classification_Type[mockLaneBoundary.classification.type],
         }),
         expect.objectContaining({
+          key: "color",
+          value: LaneBoundary_Classification_Color[mockLaneBoundary.classification.color],
+        }),
+        expect.objectContaining({
           key: "width",
           value: mockLaneBoundaryPoint.width.toString(),
+        }),
+        expect.objectContaining({
+          key: "height",
+          value: mockLaneBoundaryPoint.height.toString(),
         }),
       ]),
     );
