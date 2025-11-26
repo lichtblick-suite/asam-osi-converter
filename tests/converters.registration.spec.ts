19a75e7f08d6d4c760091b20231c168fc3ae4b1c
Thomas Sedlmayer <tsedlmayer@pmsfit.de>
2025-11-26 16:57:17 +0100

    test: reorganize tests
    Signed-off-by: Thomas Sedlmayer <tsedlmayer@pmsfit.de>

--- tests/converters.registration.spec.ts
+++ tests/converters.registration.spec.ts
@@ -1,18 +1,12 @@
-import { buildLaneBoundaryMetadata } from "@features/lanes/metadata";
-import { buildMovingObjectMetadata } from "@features/movingobjects/metadata";
 import {
   GroundTruth,
   Lane,
   Lane_Classification_Type,
   LaneBoundary,
-  LaneBoundary_BoundaryPoint,
   LaneBoundary_Classification_Color,
   LaneBoundary_Classification_Type,
   MovingObject,
   MovingObject_Type,
-  MovingObject_VehicleClassification_LightState_BrakeLightState,
-  MovingObject_VehicleClassification_LightState_GenericLightState,
-  MovingObject_VehicleClassification_LightState_IndicatorState,
   MovingObject_VehicleClassification_Type,
   StationaryObject,
 } from "@lichtblick/asam-osi-types";
@@ -211,122 +205,5 @@ describe("OSI Visualizer: Message Converter", () => {
     const result = messageConverterArgs.converter(mockMessageData);
     expect(result.deletions).toBeDefined();
     expect(result.entities).toBeDefined();
-  });
-});
-
-describe("OSI Visualizer: Moving Objects", () => {
-  const input = {
-    type: MovingObject_Type.VEHICLE,
-    base: {
-      acceleration: {
-        x: 20,
-        y: 20,
-        z: 0,
-      },
-      velocity: {
-        x: 30,
-        y: 40,
-        z: 0,
-      },
-    },
-    moving_object_classification: {
-      assigned_lane_id: [99, 100],
-    },
-    vehicle_classification: {
-      type: 5,
-      light_state: {
-        indicator_state: 5,
-        brake_light_state: 4,
-        head_light: 3,
-      },
-    },
-  } as unknown as DeepRequired<MovingObject>;
-
-  it("builds metadata  for vehicle moving objects", () => {
-    expect(buildMovingObjectMetadata(input)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          key: "moving_object_type",
-          value: MovingObject_Type[input.type],
-        }),
-        expect.objectContaining({
-          key: "acceleration",
-          value: `${input.base.acceleration.x}, ${input.base.acceleration.y}, ${input.base.acceleration.z}`,
-        }),
-        expect.objectContaining({
-          key: "velocity",
-          value: `${input.base.velocity.x}, ${input.base.velocity.y}, ${input.base.velocity.z}`,
-        }),
-        expect.objectContaining({
-          key: "assigned_lane_id",
-          value: input.moving_object_classification.assigned_lane_id
-            .map((id) => id.value)
-            .join(","),
-        }),
-        expect.objectContaining({
-          key: "type",
-          value: MovingObject_VehicleClassification_Type[input.vehicle_classification.type],
-        }),
-        expect.objectContaining({
-          key: "light_state.indicator_state",
-          value:
-            MovingObject_VehicleClassification_LightState_IndicatorState[
-              input.vehicle_classification.light_state.indicator_state
-            ],
-        }),
-        expect.objectContaining({
-          key: "light_state.brake_light_state",
-          value:
-            MovingObject_VehicleClassification_LightState_BrakeLightState[
-              input.vehicle_classification.light_state.brake_light_state
-            ],
-        }),
-        expect.objectContaining({
-          key: "light_state.head_light",
-          value:
-            MovingObject_VehicleClassification_LightState_GenericLightState[
-              input.vehicle_classification.light_state.head_light
-            ],
-        }),
-      ]),
-    );
-  });
-});
-
-describe("OSI Visualizer: Lane Boundaries", () => {
-  it("builds metadata for lane boundaries", () => {
-    const mockLaneBoundaryPoint = {
-      position: { x: 0, y: 0, z: 0 },
-      width: 2.0,
-      height: 0.0,
-    } as DeepRequired<LaneBoundary_BoundaryPoint>;
-    const mockLaneBoundary = {
-      id: { value: 123 },
-      classification: {
-        type: LaneBoundary_Classification_Type.SOLID_LINE,
-      },
-      boundary_line: [mockLaneBoundaryPoint],
-    } as DeepRequired<LaneBoundary>;
-
-    expect(buildLaneBoundaryMetadata(mockLaneBoundary)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          key: "type",
-          value: LaneBoundary_Classification_Type[mockLaneBoundary.classification.type],
-        }),
-        expect.objectContaining({
-          key: "color",
-          value: LaneBoundary_Classification_Color[mockLaneBoundary.classification.color],
-        }),
-        expect.objectContaining({
-          key: "width",
-          value: mockLaneBoundaryPoint.width.toString(),
-        }),
-        expect.objectContaining({
-          key: "height",
-          value: mockLaneBoundaryPoint.height.toString(),
-        }),
-      ]),
-    );
   });
 });
