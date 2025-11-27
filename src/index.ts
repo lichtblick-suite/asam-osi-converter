b5cd7837a334d467efae7894722ff23bffd3f5cd
Thomas Sedlmayer <tsedlmayer@pmsfit.de>
2025-11-27 11:23:35 +0100

    refactor: split out panel settings generation for reuse
    Signed-off-by: Thomas Sedlmayer <tsedlmayer@pmsfit.de>

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,24 @@ import {
   registerGroundTruthConverter,
   convertSensorDataToSceneUpdate,
   registerSensorViewConverter,
-  GroundTruthPanelSettings,
 } from "@converters";
 import { preloadDynamicTextures } from "@features/trafficsigns";
 import { SensorView } from "@lichtblick/asam-osi-types";
-import { ExtensionContext, PanelSettings } from "@lichtblick/suite";
+import { ExtensionContext } from "@lichtblick/suite";
+
+import { generateGroundTruth3DPanelSettings } from "./converters/groundTruth/panelSettings";
 
 export function activate(extensionContext: ExtensionContext): void {
   preloadDynamicTextures();
-
-  const generatePanelSettings = <T>(obj: PanelSettings<T>) => obj as PanelSettings<unknown>;
 
   const groundTruthConverter = registerGroundTruthConverter();
   extensionContext.registerMessageConverter({
     fromSchemaName: "osi3.GroundTruth",
     toSchemaName: "foxglove.SceneUpdate",
     converter: groundTruthConverter,
+    panelSettings: {
+      "3D": generateGroundTruth3DPanelSettings(),
+    },
   });
 
   const sensorViewConverter = registerSensorViewConverter();
@@ -27,84 +29,7 @@ export function activate(extensionContext: ExtensionContext): void {
     toSchemaName: "foxglove.SceneUpdate",
     converter: sensorViewConverter,
     panelSettings: {
-      "3D": generatePanelSettings({
-        settings: (config) => ({
-          fields: {
-            caching: {
-              label: "Caching",
-              input: "boolean",
-              value: config?.caching,
-              help: "Enables caching of lanes and lane boundaries.",
-            },
-            showAxes: {
-              label: "Show axes",
-              input: "boolean",
-              value: config?.showAxes,
-            },
-            showPhysicalLanes: {
-              label: "Show Physical Lanes",
-              input: "boolean",
-              value: config?.showPhysicalLanes,
-            },
-            showLogicalLanes: {
-              label: "Show Logical Lanes",
-              input: "boolean",
-              value: config?.showLogicalLanes,
-            },
-            showBoundingBox: {
-              label: "Show Bounding Box",
-              input: "boolean",
-              value: config?.showBoundingBox,
-            },
-            show3dModels: {
-              label: "Show 3D Models",
-              input: "boolean",
-              value: config?.show3dModels,
-            },
-            defaultModelPath: {
-              label: "Default 3D Model Path",
-              input: "autocomplete",
-              value: config?.defaultModelPath,
-              items: [],
-            },
-          },
-        }),
-        handler: (action, config: GroundTruthPanelSettings | undefined) => {
-          if (config == undefined) {
-            return;
-          }
-          if (action.action === "update" && action.payload.path[2] === "caching") {
-            config.caching = action.payload.value as boolean;
-          }
-          if (action.action === "update" && action.payload.path[2] === "showAxes") {
-            config.showAxes = action.payload.value as boolean;
-          }
-          if (action.action === "update" && action.payload.path[2] === "showPhysicalLanes") {
-            config.showPhysicalLanes = action.payload.value as boolean;
-          }
-          if (action.action === "update" && action.payload.path[2] === "showLogicalLanes") {
-            config.showLogicalLanes = action.payload.value as boolean;
-          }
-          if (action.action === "update" && action.payload.path[2] === "showBoundingBox") {
-            config.showBoundingBox = action.payload.value as boolean;
-          }
-          if (action.action === "update" && action.payload.path[2] === "show3dModels") {
-            config.show3dModels = action.payload.value as boolean;
-          }
-          if (action.action === "update" && action.payload.path[2] === "defaultModelPath") {
-            config.defaultModelPath = action.payload.value as string;
-          }
-        },
-        defaultConfig: {
-          caching: true,
-          showAxes: true,
-          showPhysicalLanes: true,
-          showLogicalLanes: false,
-          showBoundingBox: true,
-          show3dModels: false,
-          defaultModelPath: "/opt/models/vehicles/",
-        },
-      }),
+      "3D": generateGroundTruth3DPanelSettings(),
     },
   });
 
